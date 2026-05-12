937e28c3d30c18ce4698af72f126a34114468982
Aga Furgalska <aga.furgalska@wp.pl>
2026-05-12 19:55:40 +0200

    fix(growingSetup): changed growing setup card to fetch sensor readings
    Took 48 seconds

--- app/features/growingSetups/components/growing-setup-card.tsx
+++ app/features/growingSetups/components/growing-setup-card.tsx
@@ -1,23 +1,44 @@
+import type {SetupReading} from "~/features/growingSetups/types";
+import {useEffect, useState} from "react";
+import {growingSetupsService} from "~/features/growingSetups/service/growingSetupsService";
+
 type GrowingSetupCardProps = {
+    setupId: number;
   name: string;
-  temperature: number;
-  humidity: number;
-  light: number;
 };
 
 export default function GrowingSetupCard({
+                                             setupId,
   name,
-  temperature,
-  humidity,
-  light,
 }: GrowingSetupCardProps) {
+
+    const [loading, setLoading] = useState(true);
+    const [readingsError, setReadingsError] = useState<string | null>(null);
+    const [setupReadings, setSetupReadings] = useState<SetupReading | undefined>(undefined)
+
+    useEffect(() => {
+        const fetchReadings = async () => {
+            const readings = await growingSetupsService.getSetupSensorReadings(setupId);
+            setSetupReadings(readings);
+        };
+
+        fetchReadings();
+
+    }, [loading, setupReadings]);
+
   return (
     <div>
       <h1>{name}</h1>
 
-      <div>Temperature: {temperature}°</div>
-      <div>Humidity: {humidity}%</div>
-      <div>Light: {light}%</div>
+        {setupReadings ? (
+            <>
+                <div>Temperature: {setupReadings.temperature}°</div>
+                <div>Humidity: {setupReadings.humidity}%</div>
+                <div>Light: {setupReadings.light}%</div>
+            </>
+        ) : (
+            <div>No readings available</div>
+        )}
     </div>
   );
 }
