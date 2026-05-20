0d2df581b93e6ffa6b53ee92198ea2018fb5a03e
Alexandrin Solomca <alexsolomca169@gmail.com>
2026-05-21 01:32:09 +0200

    dev merge

--- app/routes/plant.tsx
+++ app/routes/plant.tsx
@@ -1,30 +1,8 @@
-import type { Route } from "./+types/plant";
-
-import PlantCard from "../features/plants/plant-card/plant-card";
-
-export async function clientLoader({
-  params,
-}: Route.LoaderArgs) {
-
-  const plantId = params.plantId;
-
-  return { plantId };
-}
+import { useParams } from "react-router";
+import { PlantLayout } from "~/components/plant/plant-subpages/plant-layout";
 
 export default function PlantRoute() {
+  const { plantId } = useParams<{ plantId: string }>();
 
-  return (
-
-    <PlantCard
-      name="Sweet basil"
-      type="Lamiaceae"
-      lastWatered="6h"
-      sunlight="6.4 hrs"
-      moisture={62}
-      temperature={22}
-      humidity={64}
-      light={68}
-    />
-
-  );
+  return <PlantLayout plantId={plantId ?? ""} />;
 }
